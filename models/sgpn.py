aa6b5df38e3fa3c0cf8c0ab5f72044e51e5803b7
Shreyas Misra <shreyasmisra13@gmail.com>
2022-12-17 17:47:30 +0530

    update

--- models/sgpn.py
+++ models/sgpn.py
@@ -13,12 +13,14 @@ from models.layers import build_mlp
 from lib.config import CONF
 
 PRETRAINED_DGCNN = "/home/vrlab725_wff/Documents/segmentation/dgcnn/pytorch/pretrained/model.1024.t7"
+DEVICE = "cuda"
 
 def get_num_params(model):
     model_parameters = filter(lambda p: p.requires_grad, model.parameters())
     num_params = int(sum([np.prod(p.size()) for p in model_parameters]))
 
     return num_params
+
 
 def load_pretained_cls_model(model):
     # load pretrained pointnet_cls model [ relPointNet ver. ]
@@ -27,6 +29,43 @@ def load_pretained_cls_model(model):
     pretrained_dict_ = {k[5:]: v for k, v in pretrained_dict.items() if 'feat' in k and v.size() == net_state_dict[k[5:]].size()}
     net_state_dict.update(pretrained_dict_)
     model.load_state_dict(net_state_dict)
+
+
+def knn(x, k):
+    inner = -2*torch.matmul(x.transpose(2, 1), x)
+    xx = torch.sum(x**2, dim=1, keepdim=True)
+    pairwise_distance = -xx - inner - xx.transpose(2, 1)
+
+    idx = pairwise_distance.topk(k=k, dim=-1)[1]   # (batch_size, num_points, k)
+    return idx
+
+
+def get_graph_feature_edge(x, k=20, idx=None, dim9=False):
+    batch_size = x.size(0)
+    num_points = x.size(2)
+    x = x.view(batch_size, -1, num_points)
+    if idx is None:
+        if dim9 == False:
+            idx = knn(x, k=k)  # (batch_size, num_points, k)
+        else:
+            idx = knn(x[:, 6:], k=k)
+
+    idx_base = torch.arange(0, batch_size, device=DEVICE).view(-1, 1, 1) * num_points
+    indices = idx + idx_base
+
+    indices = indices.view(-1)
+    _, num_dims, _ = x.size()
+
+    x = x.transpose(2, 1).contiguous()  # (batch_size, num_points, num_dims)  -> (batch_size*num_points, num_dims) #   batch_size * num_points * k + range(0, batch_size*num_points)
+    feature = x.view(batch_size * num_points, -1)[indices, :]
+    feature = feature.view(batch_size, num_points, k, num_dims)
+    x = x.view(batch_size, num_points, 1, num_dims).repeat(1, 1, k, 1)
+    feature = (feature - x).permute(0, 3, 1, 2).contiguous()
+    base = torch.repeat_interleave(torch.arange(0, 21, device=DEVICE), repeats=k)
+    base = torch.repeat_interleave(base.unsqueeze(dim=1), repeats=batch_size, dim=1).T
+    idx = torch.stack((base, idx.permute(0, 2, 1).reshape(batch_size, -1)), dim=2)
+    return feature, idx  # (batch_size, num_dims, num_points, k)
+
 
 class SGPN(nn.Module):
     def __init__(self, use_pretrained_cls, feature_extractor='pointnet', graph_gen='gcn', gnn_dim=256, gnn_hidden_dim=512,
@@ -38,9 +77,9 @@ class SGPN(nn.Module):
         assert feature_extractor == 'pointnet' or feature_extractor == 'dgcnn'
         self.feature_extractor = feature_extractor
         if feature_extractor == 'pointnet':
-            self.objExtractor = PointNetEncoder(global_feat=True, feature_transform=True, channel=3)  # (x,y,z)
+            self.objExtractor = PointNetEncoder(global_feat=True, feature_transform=True, channel=6)  # (x,y,z)
             self.relExtractor = PointNetEncoder(global_feat=True, feature_transform=True,
-                                                channel=4)  # (x,y,z,M) M-> class-agnostic instance segmentation
+                                                channel=3)  # (x,y,z,M) M-> class-agnostic instance segmentation
             if use_pretrained_cls:
                 print("Loading pretrained classifier ...")
                 load_pretained_cls_model(self.objExtractor)
@@ -95,21 +134,20 @@ class SGPN(nn.Module):
     def forward(self, data_dict):
         # objects_id = data_dict["objects_id"]
         objects_pc = data_dict["objects_pc"]
-
-        # TODO: check the shape of objects_cat. Num unique objects in scene. Should be a number.
-        objects_count = data_dict["objects_cat"].unique() #data_dict["aligned_obj_num"]   # namely 9
-        predicate_pc_flag = data_dict["poses"] #data_dict["predicate_pc_flag"]
-        predicate_count = data_dict["aligned_rel_num"] # namely 72
+        objects_count = data_dict["objects_cat"].flatten().unique().shape[0] 
+        predicate_pc_flag = data_dict["poses"] # 1, 3, 25, 25
+        predicate_count = data_dict["triples"][:,:,-1].flatten().unique().shape[0] 
         edges = data_dict["edges"]
         trans_feat = []
-        batch_size = objects_pc.size(0) // 20
+        batch_size = objects_pc.size(0) // 60
 
         # point cloud pass feature extractor
         objects_pc = objects_pc.permute(0, 2, 1)
-        predicate_pc_flag = predicate_pc_flag.permute(0, 2, 1)
+        predicate_pc_flag = predicate_pc_flag.flatten(2)
+        # predicate_pc_flag = predicate_pc_flag.permute(0, 2, 1)
         if self.feature_extractor == 'pointnet':
-            obj_vecs, _, tf1 = self.objExtractor(objects_pc)
-            pred_vecs, _, tf2 = self.relExtractor(predicate_pc_flag)
+            obj_vecs, _, tf1 = self.objExtractor(objects_pc) # 60, 1024 | 60, 64, 64
+            pred_vecs, _, tf2 = self.relExtractor(predicate_pc_flag) # 2, 1024 (only works with batch size > 1)
             trans_feat.append(tf1)
             trans_feat.append(tf2)
         else:
